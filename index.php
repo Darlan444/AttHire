afd57d361bf5ac1cbcda4e8295e2209ec695b2dc
Darlan444 <darlaninacio321@gmail.com>
2023-05-19 04:18:56 -0300

    header

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include 'src/view/header.php'?>
+
+    <!-- LORD ICONS -->
+    <div class="icons">
+        <!-- SEARCH -->
+        <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020"
+            style="width:2rem;height:2rem">
+        </lord-icon>
+        <!-- TRASH -->
+        <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020"
+            style="width:2rem;height:2rem">
+        </lord-icon>
+        <!-- FOLDER -->
+        <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020"
+            style="width:2rem;height:2rem">
+        </lord-icon>
+        
+        
+    </div>
+    <!-- LORD ICONS -->
+
+    
+
+</body>
+
+</html>
---
+++ src/view/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hire</title>
+
+    <link rel="stylesheet" href="src/css/style.css">
+
+    <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+
+</head>
+
+<body>
+
+    <header>
+        <nav>
+
+            <div class="home">
+                <a class="home" href="#">
+                    <!-- HOME -->
+                    <lord-icon class="icon" src="https://cdn.lordicon.com/slduhdil.json" trigger="hover" colors="primary:#fff" style="width:4rem;height:4rem">
+                    </lord-icon>
+                    <h1 class="titlelogo">HIRE</h1>
+                </a>
+            </div>
+
+            <div class="nav-itens">
+                <!-- TOGGLE THEME -->
+                <div class="div-switch">
+                    <label class="switch">
+                        <input id="checkbox-theme" type="checkbox" />
+                        <span class="slider"></span>
+                    </label>
+                </div>
+                <script src="src/js/toggleTheme.js"></script>
+                <!-- TOGGLE THEME -->
+                <!-- AVATAR -->
+                <lord-icon class="icon" src="https://cdn.lordicon.com/hbvyhtse.json" trigger="hover" colors="primary:#fff" style="width:3rem;height:3rem">
+                </lord-icon>
+            </div>
+        </nav>
+    </header>
