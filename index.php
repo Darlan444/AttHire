e4538a0c845880ae3cf33b3c9d23eda0ee2843cb
Darlan444 <darlaninacio321@gmail.com>
2023-05-21 03:45:26 -0300

    footer

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
-<?php include 'src/view/header.php'?>
+<?php include 'src/view/header.php' ?>
 
+
+<main>
     <!-- LORD ICONS -->
     <div class="icons">
         <!-- SEARCH -->
-        <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020"
-            style="width:2rem;height:2rem">
+        <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
         </lord-icon>
         <!-- TRASH -->
-        <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020"
-            style="width:2rem;height:2rem">
+        <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
         </lord-icon>
         <!-- FOLDER -->
-        <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020"
-            style="width:2rem;height:2rem">
+        <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
         </lord-icon>
-        
-        
+
     </div>
     <!-- LORD ICONS -->
+</main>
 
-    
-
-</body>
-
-</html>
+<?php include 'src/view/footer.php' ?>
---
+++ src/view/footer.php
@@ -0,0 +1,19 @@
+<div class="shadow-footer"></div>
+
+<footer>
+    <div class="div-logo-footer">
+        <img class="logo-footer" src="src/img/DS-bg-orange.png" alt="LOGO">
+    </div>
+
+    <div class="social-footer">
+        <a target="_blank" href="https://www.linkedin.com/in/darlan7s/"><i class="bx bxl-linkedin-square bx-lg"></i></a>
+        <a target="_blank" href="https://github.com/Darlan444"><i class="bx bxl-github bx-lg"></i></a>
+    </div>
+    <span class="span-footer">© 2023 Darlan Santos</span>
+</footer>
+
+
+
+</body>
+
+</html>
