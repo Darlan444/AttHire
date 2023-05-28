bb9d3411f90269e20877a56ef84fa27e94ddefda
Darlan444 <darlaninacio321@gmail.com>
2023-05-28 03:50:17 -0300

    cards

--- src/js/toggleTheme.js
+++ src/js/toggleTheme.js
@@ -1,10 +1,8 @@
 const theme = document.getElementById("checkbox-theme");
 const body = document.querySelector("body");
-const card = document.getElementById("card");
 
 const toggleThemeMode = () => {
   body.classList.toggle("dark");
 };
 
 theme.onchange = toggleThemeMode;
-
--- src/view/table.php
+++ src/view/table.php
@@ -2,6 +2,11 @@
 
     <div id="card" class="card">
         <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
+        <div>
+            <h2>TITLE</h2>
+            <p>DESCRIPTION</p>
+        </div>
+
         <!-- LORD ICONS -->
         <div class="icons">
             <!-- SEARCH -->
@@ -19,6 +24,12 @@
     </div>
     <div class="card">
         <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
+
+        <div>
+            <h2>TITLE</h2>
+            <p>DESCRIPTION</p>
+        </div>
+        
         <!-- LORD ICONS -->
         <div class="icons">
             <!-- SEARCH -->
@@ -34,87 +45,5 @@
         </div>
         <!-- LORD ICONS -->
     </div>
-    <div class="card">
-        <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
-        <!-- LORD ICONS -->
-        <div class="icons">
-            <!-- SEARCH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- TRASH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- FOLDER -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-
-        </div>
-        <!-- LORD ICONS -->
-    </div>
-    <div class="card">
-        <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
-        <!-- LORD ICONS -->
-        <div class="icons">
-            <!-- SEARCH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- TRASH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- FOLDER -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-
-        </div>
-        <!-- LORD ICONS -->
-    </div>
-    <div class="card">
-        <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
-        <!-- LORD ICONS -->
-        <div class="icons">
-            <!-- SEARCH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- TRASH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- FOLDER -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-
-        </div>
-        <!-- LORD ICONS -->
-    </div>
-    <div class="card">
-        <img class="img-card" src="./src/img/DS-bg-orange.png" alt="">
-        <!-- LORD ICONS -->
-        <div class="icons">
-            <!-- SEARCH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xfftupfv.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- TRASH -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/jmkrnisz.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-            <!-- FOLDER -->
-            <lord-icon class="icon" src="https://cdn.lordicon.com/xhcrhqyw.json" trigger="hover" colors="primary:#202020" style="width:2rem;height:2rem">
-            </lord-icon>
-
-        </div>
-        <!-- LORD ICONS -->
-    </div>
-
-
-
 
 </main>
-
-
-<script>
-    const card = document.getElementById("card");
-    const body = document.querySelector("body");
-    if (body.classList.contains("dark")) {
-        card.style.backgroundColor = "red"
-    } else {
-        card.style.backgroundColor = "white"
-    }
-</script>
